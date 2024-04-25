d0e636f1ebf828faecaa882068f13af6cad3fe98
YiboXia94 <yibo.xia@outlook.com>
2024-04-25 13:31:55 +0800

    Translated doc

--- docs/make.jl
+++ docs/make.jl
@@ -5,16 +5,17 @@ using UnPack
 # Convert workflow
 import Literate
 
-Literate.markdown(joinpath(@__DIR__, "src", "workflow.jl"), joinpath(@__DIR__, "src"); credit = false)
+Literate.markdown(joinpath(@__DIR__, "src", "workflow.jl"), joinpath(@__DIR__, "src"); credit=false)
+
 
 pages = [
-    "Introduction" => "index.md",
-    "DrWatson Workflow Tutorial" => "workflow.md",
-    "Project Setup" => "project.md",
-    "Naming Simulations" => "name.md",
-    "Saving Tools" => "save.md",
-    "Running & Listing Simulations" => "run&list.md",
-    "Real World Examples" => "real_world.md"
+    "简介" => "index_zh.md",
+    "工作流简介" => "workflow.md",
+    "项目设置" => "project_zh.md",
+    "命名你的模拟" => "name_zh.md",
+    "保存工具" => "save_zh.md",
+    "运行和列表" => "run&list_zh.md",
+    "现实例子" => "real_world_zh.md"
 ]
 
 import Downloads
@@ -25,5 +26,5 @@ Downloads.download(
 include("build_docs_with_style.jl")
 
 build_docs_with_style(pages, DrWatson, UnPack;
-    expandfirst = ["index.md"], warnonly = true,
+    expandfirst=["index.md"], warnonly=true,
 )
